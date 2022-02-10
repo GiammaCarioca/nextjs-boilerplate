fe34444987befe99127d7be34b56d4264b844e91
Thiago Giammattey <giammattey@gmail.com>
2022-02-10 10:20:35 -0300

    add PWA

--- next.config.js
+++ next.config.js
@@ -1,3 +1,15 @@
-module.exports = {
-  reactStrictMode: true
-}
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withPWA = require('next-pwa')
+const isProd = process.env.NODE_ENV === 'production'
+
+// module.exports = {
+//   reactStrictMode: true
+// }
+
+module.exports = withPWA({
+  reactStrictMode: true,
+  pwa: {
+    dest: 'public',
+    disable: !isProd
+  }
+})
---
+++ src/components/Main/index.tsx
@@ -0,0 +1,13 @@
+import * as Styled from './styles'
+
+const Main = () => (
+  <Styled.Wrapper>
+    <Styled.Title>Boilerplate</Styled.Title>
+    <Styled.Description>
+      A simple project starter to work with TypeScript, React, NextJS and Styled
+      Components
+    </Styled.Description>
+  </Styled.Wrapper>
+)
+
+export default Main
